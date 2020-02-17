a99e4c9b4600203733db58a9f96ff91f852ea06d
Liran Cohen <iliran11@gmail.com>
2020-02-17 17:23:28 +0200

    rename function

--- directories.zsh
+++ directories.zsh
@@ -1,10 +1,10 @@
-function moveDirectory() {
+function changeCodeContext() {
   cd $1 &&
     code $1 -r
 }
 
-alias codezsh='moveDirectory ~/.oh-my-zsh/custom/'
-alias codelp='moveDirectory ~/Projects/bookaway-platform/landing-page'
-alias codebutler='moveDirectory ~/Projects/bookaway-platform/butler'
-alias coderoutes='moveDirectory ~/Projects/bookaway-platform/routes-service'
-alias codeauth='moveDirectory ~/Projects/bookaway-platform/auth'
+alias codezsh='changeCodeContext ~/.oh-my-zsh/custom/'
+alias codelp='changeCodeContext ~/Projects/bookaway-platform/landing-page'
+alias codebutler='changeCodeContext ~/Projects/bookaway-platform/butler'
+alias coderoutes='changeCodeContext ~/Projects/bookaway-platform/routes-service'
+alias codeauth='changeCodeContext ~/Projects/bookaway-platform/auth'
