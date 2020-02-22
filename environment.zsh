cdebd54e2fd60ce31f11f209d3d7f8afb2203544
Liran Cohen <iliran11@gmail.com>
2020-02-22 09:51:15 +0200

    change environment

--- environment.zsh
+++ environment.zsh
@@ -1,6 +1,6 @@
 export NPM_TOKEN="9beb0faf-94f0-41f1-b4e6-d94fa0cf22b7"
-export BW_INSTALL_PATH="/Users/lirancohen/Projects/bookaway-platform"
-export GOOGLE_APPLICATION_CREDENTIALS=/Users/lirancohen/.config/gcloud/application_default_credentials.json
+export BW_INSTALL_PATH="~/Projects/bookaway-platform"
+export GOOGLE_APPLICATION_CREDENTIALS=~/.config/gcloud/application_default_credentials.json
 export NODE_ENV=development
 export PAYMENT_PORT=3004
 export PAYMENT_DEBUG_PORT=4004
@@ -8,9 +8,9 @@ export N_PREFIX=~/n
 export PATH=$N_PREFIX/bin:$PATH
 
 PATH="$PATH:$HOME/.rvm/bin;"
-PATH="$PATH:/Users/lirancohen/bin"
+PATH="$PATH:~/bin"
 
-source /Users/lirancohen/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /Users/lirancohen/.oh-my-zsh/custom/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+source ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
