6fdc1b97b2dbdf14b597349ff7a4d2c9749ee769
Liran Cohen <iliran11@gmail.com>
2020-05-01 09:34:38 +0300

    fix scripts

--- scripts/copy-child-theme.sh
+++ scripts/copy-child-theme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ncftpput  -R -v -u $USER -p $PASSWORD $HOST \
-'/alexmedia.co.il/public_html/wp-content/themes/alexmedia-selfer/' \
-'/Users/lirancohen/Projects/personal/alexmedia-wordpress/child-theme'
+'/alexmedia.co.il/public_html/wp-content/themes/' \
+'/Users/lirancohen/Projects/personal/alexmedia-wordpress/alexmedia-selfer'
 
--- scripts/copy-menu-item-with-icon.sh
+++ scripts/copy-menu-item-with-icon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ncftpput  -R -v -u $USER -p $PASSWORD $HOST \
-'/alexmedia.co.il/public_html/wp-content/plugins/menu-item-with-icon/' \
-'/Users/lirancohen/Projects/personal/alexmedia-wordpress/child-theme'
+'/alexmedia.co.il/public_html/wp-content/plugins/' \
+'/Users/lirancohen/Projects/personal/alexmedia-wordpress/menu-item-with-icon'
 
